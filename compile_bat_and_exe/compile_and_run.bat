bbeb78eccc8b0de45e2630fbfee356933ac60899
Jake Le Breton <oniontaco992@gmail.com>
2025-01-17 23:51:33 +1100

    changed around some minor stuff and added an extra directory for compiler batch files and the .exe

---
+++ compile_bat_and_exe/compile_and_run.bat
@@ -0,0 +1,15 @@
+cd ..
+
+gcc -c misc_maths.c hex_octal_conversion.c powers_and_exponents.c rng.c statistical_operations.c gcd_lcm.c misc.c guessing_game.c temperature_conversion.c length_conversion.c
+
+gcc main.c misc_maths.o hex_octal_conversion.o powers_and_exponents.o rng.o statistical_operations.o gcd_lcm.o misc.o guessing_game.o temperature_conversion.o length_conversion.o -o multi.exe
+
+del *.o
+
+move multi.exe compile_bat_and_exe\multi.exe
+
+cd compile_bat_and_exe
+
+cls
+
+multi.exe
