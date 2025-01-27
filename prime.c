b1c99462c70df4fe7d3721a6346706a9b44b70c6
Jake Le Breton <oniontaco992@gmail.com>
2025-01-28 00:44:40 +1100

    Added a prime number calculator

---
+++ prime.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+#define MAX_ARRAY_LIMIT 100
+
+void is_prime(void)
+{
+    long int list[MAX_ARRAY_LIMIT];
+    int check[MAX_ARRAY_LIMIT] = {0};
+    const char* is_num_prime[MAX_ARRAY_LIMIT];
+    int i, n, m, count = 0;
+    char ch;
+
+    printf("\n\nEnter a list of numbers, up to %d numbers: ", MAX_ARRAY_LIMIT);
+
+    for (i = 0; i < MAX_ARRAY_LIMIT; i++)
+    {
+        if (scanf("%ld", &list[i]) != 1)
+        {
+            if (scanf("%c", &ch) == 1 && (ch == 't' || ch == 'T'))
+            {
+                break;
+            }
+            else
+            {
+                printf("Invalid input. Enter either a number or t to exit: ");
+                i--;
+                count--;
+                continue;
+            }
+        }
+
+        count++;
+    }
+
+    for (i = 0; i < count; i++) {
+        n = list[i];
+
+        for (m = n - 1; m > 1; m--) {
+            if (n % m == 0) {
+                check[i] = 1;
+                break;
+            }
+        }
+
+        if (n != 1 && check[i] == 0) {
+            check[i] = 0;
+        }
+    }
+
+    for (i = 0; i < count; i++)
+    {
+        if (check[i] == 0)
+        {
+            is_num_prime[i] = "Prime";
+        } else if (check[i] == 1) {
+            is_num_prime[i] = "Not Prime";
+        }
+    }
+
+    for (i = 0; i < count; i++)
+    {
+        printf("\n%d.   %2.0ld: %s", i + 1, list[i], is_num_prime[i]);
+    }
+
+    printf("\n\n");
+}
---
+++ prime.h
@@ -0,0 +1,6 @@
+#ifndef PRIME_H
+#define PRIME_H
+
+void is_prime(void);
+
+#endif
